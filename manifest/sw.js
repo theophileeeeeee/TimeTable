dc5f6ce9d4f9b34d7e436b23db1aa4045a365039
Théophile Masson <theophile.masson13@gmail.com>
2026-05-13 15:33:08 +0200

    bugfix

--- manifest/sw.js
+++ manifest/sw.js
@@ -1,17 +1,19 @@
-const CACHE_NAME = 'timetable-v1';
+const CACHE_NAME = 'timetable-v2';
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './android-chrome-192x192.png',
-  './android-chrome-512x512.png'
+  '/Timetable/',
+  '/Timetable/index.html',
+  '/Timetable/manifest/site.webmanifest',
+  '/Timetable/android-chrome-192x192.png',
+  '/Timetable/android-chrome-512x512.png'
 ];
+
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
+
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
@@ -20,6 +22,7 @@ self.addEventListener('activate', e => {
   );
   self.clients.claim();
 });
+
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(cached => cached || fetch(e.request))
